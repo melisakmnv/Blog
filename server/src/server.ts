0f50c6e3cba76f3ad526562e8f197689497b2f5e
Ekaraja <melisakmnv@gmail.com>
2025-04-09 18:53:54 +0700

    installed dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
+import { connectDB } from "./config/db";
 
+dotenv.config()
+connectDB()
 
 const app = express();
 
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("Hello");
 });
 
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 
 
