9afd287b5305370b1cd412a1c48c15e17aa201ec
M <melisakmnv@gmail.com>
2025-04-17 21:08:19 +0700

    created useSavePost hook

--- client/src/components/button/SaveButton.tsx
+++ client/src/components/button/SaveButton.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
+
+import { useSavePost } from "@/hooks/useSavePost"
+
 import { BsBookmark, BsBookmarkFill } from "react-icons/bs"
+
 import { Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
 import { Button } from "@/components/ui/button"
-import { useMutation } from "@tanstack/react-query"
-import { savePost, unsavePost } from "@/api/requests/user"
-import { toast } from "react-toastify"
 
 
 interface SaveButtonProps {
@@ -12,51 +13,20 @@ interface SaveButtonProps {
     isSaved: boolean;
 }
 export const SaveButton = ({ postId, isSaved }: SaveButtonProps) => {
+
     const [saved, setSaved] = useState(isSaved)
-
-    const saveMutation = useMutation({
-        mutationFn: (postId: string) => savePost(postId),
-        onSuccess: (data) => {
-            toast.success(data.message);
-        },
-        onError: (error: any) => {
-            console.log(error.response?.data)
-            toast.error(error?.response?.data?.message || "Something went wrong");
-        },
-    })
-
-    const unsaveMutation = useMutation({
-        mutationFn: (postId: string) => unsavePost(postId),
-        onSuccess: (data) => {
-            toast.success(data.message);
-        },
-        onError: (error: any) => {
-            console.log(error.response?.data)
-            toast.error(error?.response?.data?.message || "Something went wrong");
-        },
-    })
-
-    const handleSave = (postId: string) => {
-        saveMutation.mutate(postId)
-        setSaved(true)
-    }
-
-    const handleUnsave = (postId: string) => {
-        unsaveMutation.mutate(postId)
-        setSaved(false)
-    }
-
+    const { handleSave, handleUnsave, isSaving, isUnsaving } = useSavePost();
 
     return (
         <Tooltip>
             <TooltipTrigger asChild>
                 {
                     saved ? (
-                        <Button variant={"ghost"} onClick={() => handleUnsave(postId)}>
+                        <Button disabled={isUnsaving} variant={"ghost"} onClick={() => handleUnsave(postId, setSaved)}>
                             <BsBookmarkFill className="text-[#bba07f]" />
                         </Button>
                     ) : (
-                        <Button variant={"ghost"} onClick={() => handleSave(postId)}>
+                        <Button disabled={isSaving} variant={"ghost"} onClick={() => handleSave(postId, setSaved)}>
                             <BsBookmark className="text-neutral-500" />
                         </Button>
                     )
